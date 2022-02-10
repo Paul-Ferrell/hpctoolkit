55e04a0e9a805e1f7cb8e681c72ce943c9cbcab2
blue42u <anderson.jonathonm@gmail.com>
2022-02-09 23:06:57 -0600

    Use a normal abort for the HPCRUN_ABORT_TIMEOUT signal
    monitor_real_abort raises a SIGABRT using the default signal handler, which
    normally causes a coredump. On systems where coredumps aren't available,
    this prevents the MPI from handling the SIGABRT itself and generating a
    backtrace, making the ABORT_TIMEOUT useless for debugging hangs.
    
    This commit adds a "normal" abort before the monitor_real_abort, so that
    any registered SIGABRT handler will still fire. The real_abort is still
    kept to ensure the process is killed by the ABORT_TIMEOUT no matter what.

--- src/tool/hpcrun/main.c
+++ src/tool/hpcrun/main.c
@@ -404,6 +404,7 @@ abort_timeout_handler(int sig, siginfo_t* siginfo, void* context)
   long pid = (long) getpid();
   EEMSG("hpcrun: abort timeout activated in process %ld - context pc %p", 
     pid, hpcrun_context_pc(context)); 
+  abort();
   monitor_real_abort();
   
   return 0; /* keep compiler happy, but can't get here */
